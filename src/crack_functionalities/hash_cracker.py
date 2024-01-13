ed9e58579f13b2b4cfa4da91bd7a1d7093dfee0d
tim565 <schmitz.tim@protonmail.com>
2024-01-13 21:57:25 +0100

    Added the function to find intersections between two sets which is necessary to compare passwords to crack with rainbow set.

---
+++ src/crack_functionalities/hash_cracker.py
@@ -0,0 +1,14 @@
+
+def find_intersection(cracking_set, rainbow_set):
+    """
+    Finds the intersection of two sets. This is an efficient step to find hashes to crack contained in the rainbow table.
+
+    Parameters:
+    - cracking_set (set): The set of hashes that is supposed to be cracked.
+    - rainbow_set (set): The set of hashes contained in the rainbow table.
+
+    Returns:
+    - intersection_set: The intersection of both sets.
+    """
+    intersection_set = cracking_set.intersection(rainbow_set)
+    return intersection_set
