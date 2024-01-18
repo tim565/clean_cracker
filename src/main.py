d157d4ae8217e9ea36ef7d2de7b4d0493251ff98
tim565 <schmitz.tim@protonmail.com>
2024-01-18 08:57:54 +0100

    Removed warnings, cleared file.

--- src/main.py
+++ src/main.py
@@ -1,24 +1,20 @@
 import os
-import sys
+import tkinter as tk
+
+from src.graphical_user_interface.graphical_user_interface import CleanCrackerGUI
 
 # Enable relative imports from workspace/ -> Absolute file path not necessary
 os.chdir(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from src.graphical_user_interface.graphical_user_interface import CleanCrackerGUI
-
-import tkinter as tk
-
-
 
 def run_gui():
     root = tk.Tk()
-    app = CleanCrackerGUI(root)
+    CleanCrackerGUI(root)
     root.mainloop()
 
 
 if __name__ == "__main__":
     run_gui()
-
 
 # TODO: Add Type Anotation
 # TODO: Make demo File examples more impactful
